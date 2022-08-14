e0419e4e23ff262348d0ba3a7743ff45d8915a42
EC2 Default User <ec2-user@ip-172-31-31-107.ec2.internal>
2022-08-14 11:08:39 +0000

    chech.sh modified file added

---
+++ chech.sh
@@ -0,0 +1,4 @@
+The committee, in its complaint to the Governor,
+pointed out that Varghese had the lowest research score, fixed as per the UGC norms,
+among six candidates who had applied for the post in the
+Malayalam Department in November last year.
